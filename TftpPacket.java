f052e5a7f04142ad2af6437b40c85bd683e78ed5
YelloElefant <alextrotternz@gmail.com>
2024-09-10 16:20:39 +1200

    adding error code map with get error method get error method gets the exception related to the error code of the current package
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- TftpPacket.java
+++ TftpPacket.java
@@ -1,10 +1,30 @@
 import java.net.DatagramPacket;
 import java.util.Arrays;
+import java.util.*;
+import java.io.*;
 
 public class TftpPacket {
    private byte type;
    private byte blockNumber;
    private byte[] data;
+
+   private static Map<Integer, Exception> errorCodes = Map.of(
+         0, new Exception("Not defined, see error message (if any)."),
+         1, new Exception("File not found."),
+         2, new Exception("Access violation."),
+         3, new Exception("Disk full or allocation exceeded."),
+         4, new Exception("Illegal TFTP operation."),
+         5, new Exception("Unknown transfer ID."),
+         6, new Exception("File already exists."),
+         7, new Exception("No such user."));
+
+   public Exception getError() throws InvalidPacketException {
+      if (data.length == 1) {
+         return errorCodes.get((int) data[0]);
+      }
+      throw new InvalidPacketException("Invalid error packet");
+
+   }
 
    public TftpPacket(DatagramPacket p) {
 
