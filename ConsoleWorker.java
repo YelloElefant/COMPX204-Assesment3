8c4477c5603c4197db5acae2a63b9eaf33a3b167
YelloElefant <alextrotternz@gmail.com>
2024-09-10 19:06:10 +1200

    console worker to control the std out for server
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

---
+++ ConsoleWorker.java
@@ -0,0 +1,23 @@
+
+public class ConsoleWorker extends Thread {
+    private String name;
+
+    public ConsoleWorker() {
+        this.name = "ConsoleWorker";
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            OutPutStream.clear();
+            for (TftpWorker worker : TftpServer.workers) {
+                OutPutStream.out("Worker " + worker.getLocalName() + " - " + worker.filename + " - "
+                        + worker.getPort());
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+}
