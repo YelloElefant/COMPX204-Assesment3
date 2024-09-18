e901d3a152db05b62b9e5829b8338c2c03397984
YelloElefant <alextrotternz@gmail.com>
2024-09-18 12:41:59 +1200

    TftpClient javadocs
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- TftpClient.java
+++ TftpClient.java
@@ -8,64 +8,128 @@ import java.util.*;
 // TODO: clean up all code
 // TODO: first arg change to server:port/fileName
 
+/**
+ * This class is the TftpClient program. It is used to request a file from a
+ * TftpServer using the TFTP protocol. It takes the server address, port, file
+ * path
+ * and save location as arguments. It then creates a socket to the server and
+ * sends
+ * a request for the file. It then waits for a response from the server and
+ * handles
+ * the response. It then responds to the destination and port of the reviced
+ * packet.
+ * 
+ * the client recives the file in blocks of 512 bytes and writes the data to the
+ * file
+ * in the order it is recived. If the block number is the same as the previous
+ * block number
+ * the data is not written to the file as it is a duplicate.
+ * 
+ * The arguments should be in the format server:port/filePath save
+ * location
+ * 
+ * </br>
+ * </br>
+ * <dt>Default values:</dt>
+ * <ul>
+ * <li>server: loaclhost</li>
+ * <li>port: 69</li>
+ * <li>save location: received_ + fileRequested</li>
+ * </ul>
+ * 
+ * 
+ * 
+ * @author YelloElefant
+ * @version 1.0
+ */
 public class TftpClient {
 
+    /**
+     * The DatagramSocket used to send and receive packets to and from the server.
+     * This is the DatagramSocket conected to the TftpWorker on the server that is
+     * sending the data NOT the server itself.
+     * 
+     * @see DatagramSocket
+     * @see TftpWorker
+     */
     private static DatagramSocket ds;
+
+    /**
+     * The port number of the server. This is the port number of the TftpServer on
+     * the server NOT the TftpWorker.
+     * 
+     * @see TftpServer
+     */
     private static int port = 69;
+
+    /**
+     * The InetAddress of the server. This is the InetAddress of the TftpServer on
+     * the server NOT the TftpWorker.
+     * 
+     * @see TftpServer
+     * @see InetAddress
+     */
     private static InetAddress serverAddress;
+
+    /**
+     * The port number of the server. This is the port number of the TftpWorker that
+     * is sending the data on the server NOT the TftpServer.
+     */
     private static int serverPort = 0;
+
+    /**
+     * The complete path to the file to be requested from the server.
+     */
     private static String filename;
+
+    /**
+     * The location to save the file to. This is the location on the client machine
+     * where the file will be saved.
+     * 
+     * this initilised as "received_" that way if a save loaction is not provided it
+     * will save the file in the current directory with the name "received_" + the
+     * name of the file requested.
+     */
     private static String saveLocation = "received_";
+
+    /**
+     * The buffer to hold the data from the server. This is the buffer that holds
+     * the data from the server to be written to the file. this is the previous data
+     * packet
+     * that was recieved. This is used to check if the block number is the same as
+     * the previous
+     * block number. If the block number is the same the data is not written to the
+     * file as it is a duplicate.
+     * else the data is written to the file.
+     */
     private static byte[] repsonseBuffer;
 
+    /**
+     * This is the main method of the TftpClient program. It takes the server
+     * address, port, file path and save location as arguments. It then creates a
+     * socket to the server and sends a request for the file. It then waits for a
+     * response from the server and handles the response. If the response is an
+     * error packet it prints the error message to the console. If the response is
+     * the end of the file it exits the program. If the response is a data packet it
+     * holds the data in a buffer until the next data packet is recieved and checks
+     * if the 2 packets are differnt blocks
+     * if they are differnt it writes the first to the file. It then sends an ACK
+     * packet to the server for the block it received. If the
+     * server does not respond within 30 seconds it closes the connection.
+     * 
+     * The arguments should be in the format server:port/filePath save
+     * location
+     * 
+     * @param args the arguments passed to the program
+     */
     public static void main(String[] args) {
         try {
             if (args.length > 2 || args.length < 1) {
-                System.err.println("Usage: java TftpClient <filename> <save location>");
+                System.err.println("Usage: java TftpClient <server>:<port>/<filePath> <save location>");
                 System.exit(1);
             }
 
-            int indexOfStartOfFilePath = args[0].indexOf("/");
-            if (indexOfStartOfFilePath < 0) {
-                System.out.println("Invalid input");
-                return;
-            }
-
-            String filePath = args[0].substring(indexOfStartOfFilePath);
-            String serverAndPort = args[0].substring(0, indexOfStartOfFilePath);
-
-            String[] serverAndPortSplit = serverAndPort.split(":");
-            String server = serverAndPortSplit.length != 1 ? serverAndPortSplit[0] : "localhost";
-            port = Integer.parseInt(serverAndPortSplit.length > 1 ? serverAndPortSplit[1].split("/")[0] : "69");
-
-            int fileNameStartIndex = filePath.lastIndexOf("/") + 1;
-            String dir = fileNameStartIndex > 1 ? filePath.substring(1, fileNameStartIndex) : "";
-            String filename = fileNameStartIndex > 0 ? filePath.substring(fileNameStartIndex) : "";
-
-            serverAddress = InetAddress.getByName(server);
-            if (serverAddress == null) {
-                System.err.println("Invalid server address");
-                System.exit(1);
-            }
-
-            System.out.println("Sever: " + server);
-            System.out.println("Port: " + port);
-            System.out.println("Directory: " + dir);
-            System.out.println("Filename: " + filename);
-
-            // set save location
-            if (args.length == 2) {
-                saveLocation = args[1];
-            } else {
-                saveLocation += filename;
-            }
-
-            // tell user what file is being requested and where it is being saved
-            System.out.println("Requesting file: " + filename);
-            System.out.println("Requesting file path: " + dir);
-            System.out.println("Save location: " + saveLocation);
-            System.out.println("Server: " + server);
-            System.out.println("Port: " + port);
+            ParseArgs(args);
 
             // create socket to file requsting server
             ds = new DatagramSocket();
@@ -118,8 +182,6 @@ public class TftpClient {
                         serverPort);
 
                 Acknowledge(ackPacket);
-                System.out
-                        .println("ACK " + responseBlockNumber + " sent to: " + p.getAddress() + ":" + p.getPort());
 
             }
 
@@ -130,6 +192,89 @@ public class TftpClient {
 
     }
 
+    /**
+     * This method parses the arguments passed to the program. It checks for the
+     * correct number of arguments and then parses the server address, port, file
+     * path and save location. It then prints the server address, port, file path
+     * and save location to the console.
+     * 
+     * The first argument should be in the format server:port/filePath and the
+     * second argument should be the save location.
+     * 
+     * If the second argument is not provided the file will be saved in the current
+     * directory with "recived_" + the name of the file requested.
+     * 
+     * Once the arguments have been parsed the server address is resolved and stored
+     * in the serverAddress variable. If the server adress cant be resolved it will
+     * throw an
+     * UnknownHostException.
+     * 
+     * </br>
+     * </br>
+     * <dt>Default values:</dt>
+     * <ul>
+     * <li>server: loaclhost</li>
+     * <li>port: 69</li>
+     * <li>save location: received_ + fileRequested</li>
+     * </ul>
+     * 
+     * @param args the arguments passed to the program
+     * @throws UnknownHostException if the server address can not be resolved
+     */
+    private static void ParseArgs(String[] args) throws UnknownHostException {
+
+        int indexOfStartOfFilePath = args[0].indexOf("/");
+        if (indexOfStartOfFilePath < 0) {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        String filePath = args[0].substring(indexOfStartOfFilePath);
+        String serverAndPort = args[0].substring(0, indexOfStartOfFilePath);
+
+        String[] serverAndPortSplit = serverAndPort.split(":");
+        String server = serverAndPortSplit.length != 1 ? serverAndPortSplit[0] : "localhost";
+        port = Integer.parseInt(serverAndPortSplit.length > 1 ? serverAndPortSplit[1].split("/")[0] : "69");
+
+        int fileNameStartIndex = filePath.lastIndexOf("/") + 1;
+        String dir = fileNameStartIndex > 1 ? filePath.substring(1, fileNameStartIndex) : "";
+        filename = fileNameStartIndex > 0 ? filePath.substring(fileNameStartIndex) : "";
+        filename = dir + filename;
+        serverAddress = InetAddress.getByName(server);
+        if (serverAddress == null) {
+            System.err.println("Invalid server address");
+            System.exit(1);
+        }
+
+        // set save location
+        if (args.length == 2) {
+            saveLocation = args[1];
+        } else {
+            saveLocation += filename;
+        }
+
+        // tell user what file is being requested and where it is being saved
+        System.out.println("Requesting file: " + filename);
+        System.out.println("Requesting file path: " + dir);
+        System.out.println("Save location: " + saveLocation);
+        System.out.println("Server: " + server);
+        System.out.println("Port: " + port);
+    }
+
+    /**
+     * This method handles a {@link TftpPacket TftpPacket} and returns the block
+     * number
+     * that needs to be acknowledged.
+     * It checks for error packets and prints the error message to the console. It
+     * also checks for the end of the file
+     * and exits the program if all blocks have been received. The data from the
+     * packet is stored in the response buffer
+     * to be written to the file.
+     * 
+     * @param p a TftpPacket to be handled
+     * @return the block number of the response sent from the server
+     * @see TftpPacket
+     */
     private static byte HandleResponse(TftpPacket p) {
         // convert response to string
         byte reponseType = p.getType();
@@ -155,14 +300,22 @@ public class TftpClient {
         byte responseBlockNumber = p.getBlockNumber();
         repsonseBuffer = p.getData();
 
-        System.out.println("Block " + responseBlockNumber + " received");
-
-        // write each response block to a file
-
         // return with the block number to acknowledge
         return responseBlockNumber;
     }
 
+    /**
+     * This method sends an ACK packet to the server. It uses the
+     * {@link #Respond(DatagramPacket) Respond} method to
+     * send the Acknowledgement. This takes a packet and adds the ACK type to the
+     * start of the packet. the block numbers
+     * needs to be added to the packet before calling this method in the first spot.
+     * any exceptions are caught and printed to the console.
+     * 
+     * @param p the packet with the block number to be sent to the server
+     * @see #Respond(DatagramPacket)
+     * @see DatagramPacket
+     */
     private static void Acknowledge(DatagramPacket p) {
         try {
             byte[] ackData = new byte[2];
@@ -175,6 +328,16 @@ public class TftpClient {
         }
     }
 
+    /**
+     * This method sends a DatagramPacket to the server. the packet is the response
+     * to give.
+     * This is a helper method that can send a response at anytime with a given
+     * packet. any
+     * exceptions are caught and printed to the console.
+     * 
+     * @param p the packet to be sent to the server\
+     * @see DatagramPacket
+     */
     private static void Respond(DatagramPacket p) {
         try {
             ds.send(p);
@@ -183,6 +346,17 @@ public class TftpClient {
         }
     }
 
+    /**
+     * This method writes an array of bytes to the file specified by
+     * {@link saveLocation saveLocation}.
+     * it uses a FileOutputStream to write the data to the file with append
+     * specified
+     * as true, so that the data is appended to the end of the file.
+     * 
+     * @param data the array of bytes to be written to the file
+     * @see FileOutputStream
+     * @see saveLocation
+     */
     private static void WriteToFile(byte[] data) {
         FileOutputStream fos = null;
 
