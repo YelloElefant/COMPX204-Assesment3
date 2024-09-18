237be09347c6b67713536a62b60070632169e603
YelloElefant <alextrotternz@gmail.com>
2024-09-19 09:59:13 +1200

    removed unneeded methods, allowing port overide
    Signed-off-by: YelloElefant <alextrotternz@gmail.com>

--- TftpServer.java
+++ TftpServer.java
@@ -3,12 +3,17 @@ import java.net.DatagramSocket;
 import java.util.*;
 
 public class TftpServer {
+   /**
+    * This is the port that the TftpServer listens on. This is set to 69 by default
+    * as this is the port that TFTP servers listen on.
+    */
    private static int port = 69;
-   public static List<TftpWorker> workers = new ArrayList<TftpWorker>();
 
-   public synchronized static void editWorkerList(List<TftpWorker> edit) {
-      workers = edit;
-   }
+   /**
+    * This is a list of all the workers that are currently running. This is used to
+    * keep track of all the workers that are running.
+    */
+   public static List<TftpWorker> workers = new ArrayList<TftpWorker>();
 
    /**
     * This is the main method for the TftpServer. It binds a new DatagramSocket to
@@ -21,19 +26,29 @@ public class TftpServer {
     * @param args
     */
    public static void main(String[] args) {
+      // if there is a port number in the arguments, use that port number
+      if (args.length > 0) {
+         port = args[0].equals("") ? 69 : Integer.parseInt(args[0]);
+      }
+
       int workerCounter = 0;
+
+      // create a new DatagramSocket to listen on the port number
       try {
          DatagramSocket ds = new DatagramSocket(port);
          System.out.println("TftpServer on port " + port);
 
+         // loop forever listening for packets and creating workers to handle them
          for (;;) {
             byte[] buf = new byte[1472];
             DatagramPacket p = new DatagramPacket(buf, 1472);
 
+            // receive a packet
             ds.receive(p);
 
             workerCounter = workers.size();
 
+            // create a new worker to handle the packet received
             try {
                TftpWorker worker = new TftpWorker(p, workerCounter++);
 
